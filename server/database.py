f2614e54049671c3762a85794af46b48c06b0982
Jaiane Almeida <108701893+jaiane-almeida@users.noreply.github.com>
2022-10-12 01:38:06 -0300

    complemento  database

--- server/database.py
+++ server/database.py
@@ -5,12 +5,18 @@ import settings
 class DataBase():
     connect: AsyncIOMotorClient = None
     product_db = None
+    users_collection = None 
+    address_collection = None 
     
 database = DataBase()
 
 async def connection_db():
     database.connect = AsyncIOMotorClient(settings.settings.DATABASE_URI)
     database.product_db = database.connect.Tintas.product
+    db.users_collection = db.clients.luizaCode.users 
+    db.address_collection = db.clients.luizaCode.address
+    
+    
     
 async def connection_close():
     database.connect.close()
