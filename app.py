55b92afc59ad8f73b76db14575db63a02e535bb3
ATMI <atmirala@gmail.com>
2022-10-12 21:05:06 -0300

    fix: products

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
-import asyncio
-from cruds.product import create_product, get_products
-from server.database import connection_db
-from dotenv import load_dotenv
-load_dotenv()
+import uvicorn
+from fastapi import FastAPI
+from rotas import order_items, orders, products, user_address, address, users
+from server.database import db
 
-# Conecta com o banco
-connection_db()
+app = FastAPI()
+app.include_router(users.router)
+app.include_router(products.router)
+app.include_router(user_address.router)
+app.include_router(address.router)
+app.include_router(orders.router)
+app.include_router(order_items.router)
+app.add_event_handler("OFF", db.client.close())
 
-async def main():
-    # Cria produto
-    pr = {
-        "nome":"Teste",
-        "marca": "Suvinil",
-        "cor":"Azul",
-        "preco":150.00,
-        "externa_ou_interna":"interna",  
-     
-    }
-
-    # busca de produto
-    #product = await create_product(pr)
-    
-    produtos = await get_products()
-    print(produtos)
-
-asyncio.run(main()) 
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=8000, reload=True, access_log=False)
